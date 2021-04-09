6ce9ea11313d2a6f2a4ff1badea0293dbeb15dfe
byronKruger77 <69688959+byronKruger77@users.noreply.github.com>
2021-04-09 11:49:47 +0200

    Add files via upload

---
+++ ecommerce/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+
+
+
+
+def store(request):
+	context = {}
+
+	products = Product .objects.all()
+	context = {'products':products}
+
+	return render(request, 'ecommerce/store.html', context)
+
+def cart(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+
+	else:
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+	context = {'items':items, 'order':order}
+	return render(request, 'ecommerce/cart.html', context)
+
+def checkout(request):
+	context = {}
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+
+	else:
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+	context = {'items':items, 'order':order}
+	
+	return render(request, 'ecommerce/checkout.html', context)
+ 
