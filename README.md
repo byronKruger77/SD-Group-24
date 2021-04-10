5c1184fdc512ef9b6df1ccbe9aa1efc95f290b6b
Siphesihle1 <percysiphe@gmail.com>
2021-04-10 00:21:53 -0400

    Migrated SQLite to MySQL

---
+++ README.md
@@ -0,0 +1,8 @@
+# Database access details
+
+- *Username* : dbadmin
+- *Password* : admin12345
+- *Host* : 148.100.79.67
+- *Port* : 3306
+
+*This will give you the ability to connect to the database remotely. 
--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'main_schema',
+        'USER': 'dbadmin',
+        'PASSWORD': 'admin12345',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
